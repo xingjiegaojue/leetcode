0a8c66c2904bac96c0d2c19bdda3fd25b8dc7448
sheshiji <708867021@qq.com>
2019-12-17 12:58:49 +0800

    finish 003

--- 003_longest_substring.h
+++ 003_longest_substring.h
@@ -1,12 +1,58 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <functional>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
 
 namespace ALG_LONGEST_SUBSTRING {
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return 0;
+        int m[256];
+        memset(m,-1,sizeof(m));
+
+        unsigned int beginIndex = 0;
+        unsigned int nextIndex = 0;
+
+        int maxCount = 0;
+        while(nextIndex < s.size()) {
+            char ch = s.at(nextIndex);
+            int index = m[(int)ch];
+
+            if(index == -1) {
+                m[(int)ch] = nextIndex;
+                nextIndex++;
+            }
+
+            else {
+                int count = nextIndex - beginIndex;
+                maxCount = (maxCount < count) ? count : maxCount;
+
+                // reset -1 in between old beginIndex and newer beginIndex
+                int newBeginIndex = index + 1;
+                for(int i = beginIndex;i < newBeginIndex;i++) {
+                    m[(int)s.at(i)] = -1;
+                }
+
+                // mark current
+                m[(int)ch] = nextIndex;
+
+                // update begin
+                beginIndex = newBeginIndex;
+
+                // prepare next
+                nextIndex++;
+            }
+        }
+
+        int count = nextIndex - beginIndex;
+        maxCount = (maxCount < count) ? count : maxCount;
+
+        return maxCount;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 
+#include "001_twosum.h"
+#include "002_addtwonumbers.h"
+#include "003_longest_substring.h"
+
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << ALG_LONGEST_SUBSTRING::Solution().lengthOfLongestSubstring("abcabcbb") << endl;
+    cout << ALG_LONGEST_SUBSTRING::Solution().lengthOfLongestSubstring(" ") << endl;
+    cout << ALG_LONGEST_SUBSTRING::Solution().lengthOfLongestSubstring("abba") << endl;
     return 0;
 }
